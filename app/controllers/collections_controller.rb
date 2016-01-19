636a95c90ccb5a4e90aeb17511cf9ce98dcda176
Ivy Chang <ivycwq@icloud.com>
2016-01-18 17:49:27 -0800

    adding more functionalities

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,7 +7,24 @@ class CollectionsController < ApplicationController
   end
 
   def create
-    
+    @data = []
+    tag = params[:hashtag]
+    start_time = params[:start_time]
+    end_time = params[:end_time]
+
+    request = Typhoeus::Request.new(
+      "https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=665873981.1677ed0.93c4fb47532944059a0d132ab74646fb"
+    )
+
+    response = request.run
+    dataArray = JSON.parse(response.response_body.data)
+
+    dataArray.each |data| do
+      if data.caption.created_time > start_time && data.caption.created_time < end_time
+        @data << data
+      end
+    end
+    pry
   end
 
 end
