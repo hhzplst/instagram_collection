2da31c37b27ceee65a2466a4dafc8909aa1f84d4
Ivy Chang <ivycwq@icloud.com>
2016-01-18 23:09:26 -0800

    database

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,24 +7,33 @@ class CollectionsController < ApplicationController
   end
 
   def create
-    @data = []
     tag = params[:hashtag]
-    start_time = params[:start_time]
-    end_time = params[:end_time]
+    @start_time = Date.parse params[:start_time]
+    @end_time = Date.parse params[:end_time]
 
     request = Typhoeus::Request.new(
       "https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=665873981.1677ed0.93c4fb47532944059a0d132ab74646fb"
     )
 
     response = request.run
-    dataArray = JSON.parse(response.response_body.data)
+    @result = JSON.parse(response.response_body)
+    @dataArr = @result["data"]
 
-    dataArray.each |data| do
-      if data.caption.created_time > start_time && data.caption.created_time < end_time
-        @data << data
-      end
+    @dataArr.each do |data|
+      @curTime = data["caption"]["created_time"]
+      if Time.at(@curTime.to_i).to_date <= @start_time && Time.at(@curTime.to_i).to_date <= @end_time
+        #store in the database
+        if data["type"]==="image"
+          collection_params = {:username => data["user"]["username"], :link => data["link"], :created_time => @curTime, :image => data["images"]["thumbnail"]["url"], :video => nil }
+        else
+          collection_params = {:username => data["user"]["username"], :link => data["link"], :created_time => @curTime, :image => nil, :video => data["videos"]["low_bandwidth"]["url"] }
+        end
+        Collection.create collection_params
+        collection_params = {}
+      end 
     end
-    pry
+
+    redirect_to collections_path
   end
 
 end
